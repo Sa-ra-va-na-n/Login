75156f4a98093d42a0025eb8dfb97c6548b65afe
Sa-ra-va-na-n <sarobala28@gmail.com>
2022-04-05 16:13:07 +0530

    first commit

---
+++ src/main/java/com/ecom/login/Controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.ecom.login.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class LoginController {
+    @GetMapping("/login")
+    public void login() {
+
+    }
+}
